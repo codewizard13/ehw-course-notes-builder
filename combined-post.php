4a94b07909b02477477f5894cc2d73592d0baec7
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-02 08:34:08 -0600

    STYLES connected now

--- combined-post.php
+++ combined-post.php
@@ -5,9 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>EHD_VIDNOT_20221121_LL_JSEss_2021__WIP__01.md</title>
-    <script type="module" src="/sb/junk.js" render></script>
+    <link rel="stylesheet" href="./css/style.css"></script>
   </head>
   <body>
     <h1>EHD_VIDNOT_20221121_LL_JSEss_2021__WIP__01.md</h1>
+
+    <?php 
+    
+    define('APP_PATH', dirname(__FILE__) . '/../');
+    
+    ?>
+
+    <?php include('./post-wip/post-wip-course-js-ess-03.htm') ?>
+
   </body>
 </html>
