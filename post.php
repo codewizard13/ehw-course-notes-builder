f411d1af399c79be320edc7d3ab8791299d90868
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-02 10:26:21 -0600

    FIX BOOKMARK button styles

--- post.php
+++ post.php
@@ -4,28 +4,41 @@ require_once('app.php');
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>EHD_VIDNOT_20221121_LL_JSEss_2021__WIP__01.md</title>
-    <link rel="stylesheet" href="./css/style.css"></script>
-  </head>
-  <body class="ehw-body notes">
-    <h1>EHD_VIDNOT_20221121_LL_JSEss_2021__WIP__01.md</h1>
 
-<?php 
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>EHD_VIDNOT_20221121_LL_JSEss_2021__WIP__01.md</title>
+  <link rel="stylesheet" href="./css/style.css">
+  </script>
+</head>
 
-// include_once(APP_PATH . '/views/view-header.php');
-include_once(VIEW_PATH . '/view-header.php');
+<body>
+  <div class="ehw-wrapper">
+  <div class="ehw-body notes">
+    <div class="course-title">JavaScript Essential Training (2021)</div>
 
-include_once(VIEW_PATH . '/view--course-info.php');
-
-include_once(VIEW_PATH . '/view--course-details.php');
-
-?>
-    
+    <h1 id="course-notes">COURSE NOTES</h1>
+    <a class="bookmark-link" href="#bookmark">BOOKMARK</a>
+    <h3 id="span-class%22course-title%22javascript-essential-training-2021span"><span class="course-title">JavaScript Essential Training (2021)</span></h3>
 
 
-  </body>
+    <?php
+
+    // include_once(APP_PATH . '/views/view-header.php');
+    include_once(VIEW_PATH . '/view-header.php');
+
+    // include_once(VIEW_PATH . '/view--course-info.php');
+
+    // include_once(VIEW_PATH . '/view--course-details.php');
+
+    ?>
+
+  </div>
+
+  </div>
+
+</body>
+
 </html>
--- views/view--course-info.php
+++ views/view--course-info.php
@@ -1,1 +1,41 @@
-<h1 class="correct"><span style="color: initial">THIS FILE: </span><?php echo basename(__FILE__) ?></h1>
+<!-- <h1 class="correct"><span style="color: initial">THIS FILE: </span><?php echo basename(__FILE__) ?></h1> -->
+
+<h2 id="course-info">Course Info</h2>
+<!-- TABLE -->
+<table id="course-info-table" class="table--2-col">
+  <thead>
+    <tr>
+      <th colspan="2">JavaScript Essential Training</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>URL:</td>
+      <td><a href="https://www.linkedin.com/learning/javascript-essential-training" target="_blank" rel="noopener">https://www.linkedin.com/learning/javascript-essential-training</a></td>
+    </tr>
+    <tr>
+      <td>Channel:</td>
+      <td>LinkedIn Learning</td>
+    </tr>
+    <tr>
+      <td>Instructor:</td>
+      <td>Morten Rand-Hendriksen</td>
+    </tr>
+    <tr>
+      <td>Release Date:</td>
+      <td>2021-01-20</td>
+    </tr>
+    <tr>
+      <td>Date Started:</td>
+      <td>2022-11-21</td>
+    </tr>
+    <tr>
+      <td>Date Completed:</td>
+      <td>--</td>
+    </tr>
+    <tr>
+      <td>File Name:</td>
+      <td>EHD_VIDNOT_20221121_LL_JSEss_2021__WIP__01.md</td>
+    </tr>
+  </tbody>
+</table>
