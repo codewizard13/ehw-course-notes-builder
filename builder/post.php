bb9afa49d2ea1571da14826db22a59a73a332bf9
Eric L. Hepperle <codewizard13@gmail.com>
2023-01-04 10:52:42 -0600

    MOVE PHP post building files to builder/

---
+++ builder/post.php
@@ -0,0 +1,39 @@
+<?php
+require_once('app.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>COURSE NOTES: JavaScript Essential Training</title>
+  <link rel="stylesheet" href="./css/style.css">
+  </script>
+</head>
+
+<body>
+  <div class="ehw-wrapper">
+    <div class="ehw-body notes">
+      <div class="course-title">JavaScript Essential Training (2021)</div>
+
+      <a class="bookmark-link" href="#bookmark">BOOKMARK</a>
+
+      <?php
+
+      include_once(VIEW_PATH . '/view-header.php');
+      include_once(VIEW_PATH . '/view-toc.php');
+      include_once(VIEW_PATH . '/view-vidnotes.php');
+      include_once(VIEW_PATH . '/view-footer.php');
+
+      ?>
+
+    </div>
+
+  </div>
+
+</body>
+
+</html>
---
+++ builder/views/view-toc.php
@@ -0,0 +1,112 @@
+<!-- <h1 class="correct"><span style="color: initial">THIS FILE: </span><?php echo basename(__FILE__) ?></h1> -->
+
+<h2 id="toc">TOC</h2>
+
+<section class="accordion toc accordion-hidden">
+	
+<h3><a href="#0-introduction">0. Introduction</a></h3>
+	<h4>0.0 JavaScript: The soil from which the modern web grows</h4>
+	<h4>0.1 How to use the exercise files</h4>
+<h3><a href="#1-javascript-a-brief-introduction">1. JavaScript: A Brief Introduction</a></h3>
+	<h4>1.0 JavaScript: First contact</h4>
+	<h4>1.1 Navigating the JS landscape</h4>
+	<h4>1.2 Tools for working with JavaScript</h4>
+	<h4>1.3 Linting and formatting</h4>
+	<h4>1.4 Get to know the browser console</h4>
+	<h4>1.5 JavaScript language basics</h4>
+	<h4>1.6 Learning JavaScript backward</h4>
+	<h4>1.7 Chapter Quiz</h4>
+<h3><a href="#2-up-and-running-with-js">2. Up and Running with JS</a></h3>
+	<h4>2.0 JavaScript in an HTML document</h4>
+	<h4>2.1 JavaScript as an external file</h4>
+	<h4>2.2 Modern JavaScript loading</h4>
+	<h4>2.3 JavaScript modules</h4>
+	<h4>2.4 Chapter Quiz</h4>
+<h3><a href="#3-objects">3. Objects</a></h3>
+	<h4>3.0 Objects: A practical introduction</h4>
+	<h4>3.1 JavaScript objects: The code version</h4>
+	<h4>3.2 Object containers</h4>
+	<h4>3.3 Object properties</h4>
+	<h4>3.4 Accessing objects</h4>
+	<h4>3.5 Accessing object properties</h4>
+	<h4>3.6 Practice: Build a new object</h4>
+	<h4>3.7 Object methods</h4>
+	<h4>3.8 Practice: Build a new method</h4>
+	<h4>3.9 Classes: Object blueprints</h4>
+	<h4>3.10 Object constructors</h4>
+	<h4>3.11 Practice: Build a new object with a constructor</h4>
+	<h4>3.12 Global objects</h4>
+	<h4>3.13 Challenge: Create a new object type</h4>
+	<h4>3.14 Solution: Create a new object type</h4>
+	<h4>3.15 Chapter Quiz</h4>
+<h3>4. Sidebar: String Output</h3>
+	<h4>4.0 Mix text and variables with template literals</h4>
+	<h4>4.1 Traditional string output</h4>
+	<h4>4.2 Chapter Quiz</h4>
+<h3>5. DOM</h3>
+	<h4>5.0 DOM: The Document Object Model</h4>
+	<h4>5.1 Access elements with querySelector methods</h4>
+	<h4>5.2 Access elements using older methods</h4>
+	<h4>5.3 Practice: Find an element</h4>
+	<h4>5.4 Modifying element classes</h4>
+	<h4>5.5 Attributes</h4>
+	<h4>5.6 Inline style</h4>
+	<h4>5.7 Practice: Modify classes and attributes, and styles</h4>
+	<h4>5.8 Add DOM elements</h4>
+	<h4>5.9 Challenge: Add a new element</h4>
+	<h4>5.10 Solution: Add a new element</h4>
+	<h4>5.11 Chapter Quiz</h4>
+<h3>6. Sidebar: Variables and Data Types</h3>
+	<h4>6.0 Variables: Containers for everything</h4>
+	<h4>6.1 Var</h4>
+	<h4>6.2 Scope</h4>
+	<h4>6.3 Let</h4>
+	<h4>6.4 Const</h4>
+	<h4>6.5 Data types</h4>
+	<h4>6.6 Assignment vs. comparison</h4>
+	<h4>6.7 Math operators</h4>
+	<h4>6.8 Chapter Quiz</h4>
+<h3>7. Arrays</h3>
+	<h4>7.0 Arrays explained</h4>
+	<h4>7.1 Arrays in code</h4>
+	<h4>7.2 Array methods</h4>
+	<h4>7.3 Challenge: Build and modify an array</h4>
+	<h4>7.4 Solution: Build and modify an array</h4>
+	<h4>7.5 Chapter Quiz</h4>
+<h3>8. Functions and Methods</h3>
+	<h4>8.0 The real-world function</h4>
+	<h4>8.1 Functions and methods</h4>
+	<h4>8.2 A standard function</h4>
+	<h4>8.3 The arrow function</h4>
+	<h4>8.4 Arrow functions and "this"</h4>
+	<h4>8.5 Practice: Build a function</h4>
+	<h4>8.6 Pass data to a function with parameters</h4>
+	<h4>8.7 Return values from a function</h4>
+	<h4>8.8 Practice: Pass values between functions</h4>
+	<h4>8.9 Callbacks</h4>
+	<h4>8.10 Conditional if...else statement</h4>
+	<h4>8.11 Logical operators</h4>
+	<h4>8.12 Conditional switch statement</h4>
+	<h4>8.13 Looping through content</h4>
+	<h4>8.14 Using the map() array method</h4>
+	<h4>8.15 Challenge: Build an advanced function</h4>
+	<h4>8.16 Solution: Build an advanced function</h4>
+	<h4>8.17 Chapter Quiz</h4>
+<h3>9. Events</h3>
+	<h4>9.0 DOM events explained</h4>
+	<h4>9.1 Typical DOM events</h4>
+	<h4>9.2 Event listeners</h4>
+	<h4>9.3 Practice: Experiment with event listeners</h4>
+	<h4>9.4 Advanced event listeners and "this"</h4>
+	<h4>9.5 Pass arguments through event listeners</h4>
+	<h4>9.6 Challenge: Create an event listener</h4>
+	<h4>9.7 Solution: Create an event listener</h4>
+	<h4>9.8 Chapter Quiz</h4>
+<h3>10. Troubleshooting and Validating JS</h3>
+	<h4>10.0 Troubleshooting JavaScript in the browser</h4>
+	<h4>10.1 Making sense of a React component</h4>
+	<h4>10.2 Chapter Quiz</h4>
+<h3>11. Conclusion</h3>
+	<h4>11.0 Next steps on your learning journey</h4>
+	<h4>11.1 Chapter Quiz</h4>
+  </section>
