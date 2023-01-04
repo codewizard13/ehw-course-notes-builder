88c85e126e31e0f5cf1a0f2f22dce5c387ec3c6e
Eric L. Hepperle <codewizard13@gmail.com>
2023-01-04 10:55:10 -0600

    MOVE views/ and post.php into builder/

--- views/view-header.php
+++
@@ -1,9 +0,0 @@
-<!-- <h1 class="correct"><span style="color: initial">THIS FILE: </span><?php echo basename(__FILE__) ?></h1> -->
-
-<?php
-include_once(VIEW_PATH . '/view--course-info.php');
-include_once(VIEW_PATH . '/view--course-preview.php');
-include_once(VIEW_PATH . '/view--course-details.php');
-include_once(VIEW_PATH . '/view--course-instructor.php');
-
-
